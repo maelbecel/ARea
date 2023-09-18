981b8f5f978bd34f307af163008282a87c26adc2
Lucas <lucas1.dupont@epitech.eu>
2023-09-18 16:09:30 +0200

    build(server): Add simple example Java project

---
+++ server/src/main/java/fr/zertus/area/AreaServerApplication.java
@@ -0,0 +1,13 @@
+package fr.zertus.area;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreaServerApplication.class, args);
+	}
+
+}
