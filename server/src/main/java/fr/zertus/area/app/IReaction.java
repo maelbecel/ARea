d74161530c18b0ba201d50aadd12501596730dd0
jovanHillion <jovanhillion@gmail.com>
2023-09-26 15:28:34 +0200

    feat (./src/main/java/fr/zertus/area/app): add new method and structure to Reaction

--- server/src/main/java/fr/zertus/area/app/IReaction.java
+++ server/src/main/java/fr/zertus/area/app/IReaction.java
@@ -1,9 +1,21 @@
 package fr.zertus.area.app;
 
+import fr.zertus.area.entity.ConnectedService;
+import fr.zertus.area.utils.FormInput;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IReaction {
 
     String getName();
     String getSlug();
     String getDescription();
+    List<FormInput> getInputs();
+    Map<String, String> getPlaceholders();
+
+    public boolean setupReaction(ConnectedService service, List<FormInput> inputs);
+
+    public boolean trigger(ConnectedService service);
 
 }
--- server/src/main/java/fr/zertus/area/app/Reaction.java
+++ server/src/main/java/fr/zertus/area/app/Reaction.java
@@ -1,15 +1,27 @@
 package fr.zertus.area.app;
 
+import fr.zertus.area.entity.ConnectedService;
+import fr.zertus.area.utils.FormInput;
 import fr.zertus.area.utils.StringUtils;
+
+import java.util.List;
+import java.util.Map;
 
 public abstract class Reaction implements IReaction {
 
     private final String name;
     private final String description;
 
-    public Reaction(String name, String description) {
+    protected final List<FormInput> inputs;
+
+    protected final Map<String, String> placeholders;
+
+    public Reaction(String name, String description, List<FormInput> inputs, Map<String, String> placeholders) {
         this.name = name;
         this.description = description;
+
+        this.inputs = inputs;
+        this.placeholders = placeholders;
     }
 
     @Override
@@ -25,6 +37,26 @@ public abstract class Reaction implements IReaction {
     @Override
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public List<FormInput> getInputs() {
+        return inputs;
+    }
+
+    @Override
+    public Map<String, String> getPlaceholders() {
+        return placeholders;
+    }
+
+    @Override
+    public boolean setupReaction(ConnectedService service, List<FormInput> inputs) {
+        return false;
+    }
+
+    @Override
+    public boolean trigger(ConnectedService service) {
+        return false;
     }
 
 }
