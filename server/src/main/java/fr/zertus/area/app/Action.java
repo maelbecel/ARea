f6736b2323d1c7de5734dfdb29d904d0b547ddfc
Lucas <lucas1.dupont@epitech.eu>
2023-09-26 14:35:45 +0200

    feat(Action.java): Add Action add IAction class

--- server/src/main/java/fr/zertus/area/app/Action.java
+++ server/src/main/java/fr/zertus/area/app/Action.java
@@ -1,15 +1,28 @@
 package fr.zertus.area.app;
 
+import fr.zertus.area.entity.ConnectedService;
+import fr.zertus.area.utils.FormInput;
 import fr.zertus.area.utils.StringUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class Action implements IAction {
 
     private final String name;
     private final String description;
 
+    protected final List<FormInput> inputs;
+    protected final Map<String, String> placeholders;
+
     public Action(String name, String description) {
         this.name = name;
         this.description = description;
+
+        this.inputs = new ArrayList<>();
+        this.placeholders = new HashMap<>();
     }
 
     @Override
@@ -25,6 +38,26 @@ public abstract class Action implements IAction {
     @Override
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public List<FormInput> getInputs() {
+        return inputs;
+    }
+
+    @Override
+    public Map<String, String> getPlaceholders() {
+        return placeholders;
+    }
+
+    @Override
+    public boolean setupAction(ConnectedService service, List<FormInput> inputs) {
+        return false;
+    }
+
+    @Override
+    public boolean isTrigger(ConnectedService service) {
+        return false;
     }
 
 }
