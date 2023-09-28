19d4a1dffee7d82d1ab4e56106242a4adcfdbdde
maelbecel <mael1.becel@epitech.eu>
2023-09-28 11:12:07 +0200

    feat (screen/): add unconnected login and signin page

---
+++ mobile/Area51/components/SubmitButton.tsx
@@ -0,0 +1,64 @@
+/* The `import` statement is used to import specific modules or components from external libraries or
+files. In this case, it is importing various components and types from the `react-native` and
+`react-native-gesture-handler` libraries. */
+import { View, Text, TouchableOpacityProps, TouchableOpacity, StyleSheet, InputModeOptions, Image } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+/* The `interface InputProps` is defining the props that can be passed to the `FormInput` component.
+It extends the `TouchableOpacityProps` interface, which means it inherits all the props from that
+interface. */
+interface SubmitButtonProps extends TouchableOpacityProps {
+    title        : string;
+    icon        ?: { uri: any, width: number, height: number };
+    onPress     ?: () => void;
+}
+
+/* The code `const FormInput: React.FC<InputProps> = ({ title, secure = false, inputMode, icon,
+onChangeText, children }) => { ... }` is defining a functional component called `FormInput`. */
+const SubmitButton: React.FC<SubmitButtonProps> = ({ title, onPress, icon }) => {
+    return (
+        icon ? (
+            <TouchableOpacity style={styles.containerimg} onPress={onPress}>
+                <Text style={styles.title}>{title}</Text>
+                <Image source={icon.uri} style={{ width: icon.width, height: icon.height, marginLeft: 10 }} />
+            </TouchableOpacity>
+        ) : (
+            <TouchableOpacity style={styles.container} onPress={onPress}>
+                <Text style={styles.title}>{title}</Text>
+            </TouchableOpacity>
+        )
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 10,
+        alignItems: 'center',
+        width: '40%',
+        padding: 10,
+        backgroundColor: '#363841',
+        borderRadius: 90,
+        marginTop: 20,
+    },
+    containerimg: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10,
+        flexDirection: 'row',
+        width: '65%',
+        paddingVertical: 18,
+        paddingHorizontal: 20,
+        backgroundColor: '#363841',
+        borderRadius: 90,
+    },
+    title: {
+        color: '#FFFFFFBF',
+        fontSize: 20,
+        fontStyle: 'normal',
+        fontWeight: "700",
+    }
+});
+
+export default SubmitButton;
