02d839b0dd9887f5d02b6777f32fa078a515cc9a
BuildTools <unconfigured@null.spigotmc.org>
2023-09-28 13:33:23 +0200

    feat(Applet): Add Applet entity

---
+++ server/src/main/java/fr/zertus/area/config/RequestLoggingConfig.java
@@ -0,0 +1,18 @@
+package fr.zertus.area.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+
+}
