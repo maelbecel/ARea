f33d351955f5bd5b88115473351850ef42b78701
maelbecel <mael1.becel@epitech.eu>
2023-10-02 11:37:33 +0200

    fix (LogIn.tsx): shw error message

--- mobile/Area51/screen/SearchServices.tsx
+++ mobile/Area51/screen/SearchServices.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 
-export default function SearchServices() {
+const SearchServices = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Insert SearchServices here !</Text>
+    <View>
+      <Text>Explore</Text>
     </View>
-  );
+  )
 }
+
+export default SearchServices;
