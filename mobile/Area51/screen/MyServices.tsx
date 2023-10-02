a1baa4dc8b5667c426ca269db35df4cf3877f1f4
ZeNo <enzo.garnier@epitech.eu>
2023-10-02 11:36:23 +0200

    feat (MyApplets): start working on page myApplets

--- mobile/Area51/screen/MyServices.tsx
+++ mobile/Area51/screen/MyServices.tsx
@@ -1,10 +1,19 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Dimensions, ScrollView, KeyboardAvoidingView } from 'react-native';
 
 export default function MyServices() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Insert MyServices here !</Text>
+    <View style={styles.container}>
+      <Text>My Services</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20, // Marge à gauche et à droite de l'écran
+    paddingTop: Dimensions.get('window').height / 5 - 75,
+  },
+});
