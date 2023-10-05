343af5e5c8a511ce241f1e8d3af6629cd29a42a0
maelbecel <mael1.becel@epitech.eu>
2023-10-05 16:33:30 +0200

    feat (AddServices.tsx): add the add service page

--- mobile/Area51/screen/AddServices.tsx
+++ mobile/Area51/screen/AddServices.tsx
@@ -1,10 +1,40 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import ActionChoose from '../components/ActionChoose';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import SubmitButton from '../components/SubmitButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function AddServices() {
+const AddServices = ({navigation}) => {
+  const [action, setAction] = React.useState("default");
+  const [reaction, setReaction] = React.useState("default");
+
+  const newApplet = () => {
+    navigation.navigate("My Applets")
+  }
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const action = await AsyncStorage.getItem('action');
+        const reaction = await AsyncStorage.getItem('reaction');
+        setAction((action === null) ? "default" : action);
+        setReaction((reaction === null) ? "default" : reaction);
+      } catch (error) {
+        console.log("Error while getting info : ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Insert AddServices here !</Text>
+      <ActionChoose type="action" slug={action} onPress={() => navigation.navigate('SearchServices')} />
+      <Icon name="add-circle" size={40} color="#363841" />
+      <ActionChoose type="reaction" slug={reaction} onPress={() => navigation.navigate('SearchServices')} />
+      <SubmitButton title="Continuer" onPress={newApplet} textcolor='#FFF'/>
     </View>
   );
 }
+
+export default AddServices;
