e6de7479cec9139ee3902f99673e5a00fdb8f365
jovanHillion <jovanhillion@gmail.com>
2023-10-09 10:38:48 +0200

    rebase (./): dev on web

--- server/src/main/java/fr/zertus/area/controller/UserController.java
+++ server/src/main/java/fr/zertus/area/controller/UserController.java
@@ -8,6 +8,10 @@ import fr.zertus.area.payload.response.ApiResponse;
 import fr.zertus.area.security.utils.JwtTokenProvider;
 import fr.zertus.area.service.RegisterUserService;
 import fr.zertus.area.service.UserService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User", description = "User endpoint")
 public class UserController {
 
     @Autowired
@@ -31,6 +36,11 @@ public class UserController {
     private UserService userService;
 
 
+    @Operation(summary = "Login", description = "Login to the API", tags = { "User" })
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Return a JWT token", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class))),
+    })
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<Object>> login(@RequestBody LoginDTO login) throws IllegalAccessException {
         try {
@@ -45,26 +55,32 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Logout", description = "Logout from the API", tags = { "User" })
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "No content"),
+    })
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse<String>> logout() {
         SecurityContextHolder.clearContext();
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Register", description = "Register to the API", tags = { "User" })
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Return a JWT token", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "409", description = "Conflict", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class))),
+    })
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<User>> register(@RequestBody RegisterDTO register) {
         ApiResponse<User> response = registerUserService.register(register);
         return response.toResponseEntity();
     }
 
-    @PostMapping("/verify")
-    public ResponseEntity<ApiResponse<String>> verify(@RequestBody CheckDTO token) throws IllegalAccessException {
-        if (JwtTokenProvider.validateToken(token.getToken())) {
-            return ApiResponse.noContent().toResponseEntity();
-        }
-        throw new IllegalAccessException("Invalid token");
-    }
-
+    @Operation(summary = "User info", description = "Get the user information", tags = { "User" })
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Return the user information", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = User.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class))),
+    })
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<Object>> getCurrentUser() throws IllegalAccessException {
         try {
