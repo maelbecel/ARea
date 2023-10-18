91666e17f0fa5049f3949f67d77b638b8e44d425
Ethan <ethan1.hernou@epitech.eu>
2023-10-18 09:39:47 +0200

    update (./component/SearchService): now service are responsive

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -69,7 +69,7 @@ const IndexPage: NextPage = () => {
         {connected ? (
           <div className="flex items-center flex-col mt-[2em]">
             <div className="w-full flex justify-center items-center flex-col">
-              <h1 className="text-center font-extrabold text-[#363841] text-[2.6rem] mb-[1em] w-full">
+              <h1 className="text-center font-extrabold text-[#363841] text-[26px] md:text-[50px] mb-[1em] w-full">
                 Explore services
               </h1>
             </div>
