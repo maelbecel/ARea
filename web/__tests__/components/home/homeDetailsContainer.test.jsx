1e2812d68154848510070a714be461891a4b51d4
jovanHillion <jovanhillion@gmail.com>
2023-11-03 13:41:01 +0100

    fix (./package.json): change test librairie

--- web/__tests__/components/home/homeDetailsContainer.test.jsx
+++ web/__tests__/components/home/homeDetailsContainer.test.jsx
@@ -1,27 +1,33 @@
-import React from "react";
-import { createRoot } from 'react'
-import { act } from "react-dom/test-utils";
-import { render, unmountComponentAtNode } from "react-dom";
-import HomeDetailsContainer from "../../../components/HomePage/Container/HomeDetailsContainer.tsx";
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import HomeDetailsContainer from '../../../components/HomePage/Container/HomeDetailsContainer.tsx'
 
-let container = null;
+test('displays the main title', () => {
+    // ARRANGE
+    render(<HomeDetailsContainer />);
 
-beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement("div");
-    document.body.appendChild(container);
+    // ACT
+
+    // ASSERT
+    expect(screen.getByText('There are unlimited ways to save time')).toBeInTheDocument();
 });
 
-afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+test('displays the feature title', () => {
+    // ARRANGE
+    render(<HomeDetailsContainer />);
+
+    // ACT
+
+    // ASSERT
+    expect(screen.getByText('Cross post to multiple social networks')).toBeInTheDocument();
 });
 
-it("renders with or without a name", () => {
-    act(() => {
-        render(<HomeDetailsContainer />, container)
-    });
-    expect(container.textContent).toBe("There are unlimited ways to save timeCross post to multiple social networksSave time by writing once and posting to multiple networks automatically.");
+test('displays the feature description', () => {
+    // ARRANGE
+    render(<HomeDetailsContainer />);
+
+    // ACT
+
+    // ASSERT
+    expect(screen.getByText('Save time by writing once and posting to multiple networks automatically.')).toBeInTheDocument();
 });
